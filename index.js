1e25ff8c2b7fa0219b2c95bc30b4d5a515504756
sohail_rasool-786 <sohail.rasool@conovoinc.com>
2022-01-10 18:35:40 +0500

    Add structure for messenger app

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const multer = require('multer');
-const userRoute = require('./routes/users');
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
 dotenv.config();
 
@@ -17,39 +19,40 @@ mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   }
 );
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan('common'));
+app.use(morgan("common"));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images');
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    // cb(null, req.body.name);
-    cb(null, file.originalname);
+    cb(null, req.body.name);
   },
 });
 
 const upload = multer({ storage: storage });
-app.post('/api/upload', upload.single('file'), (req, res) => {
+app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
-    return res.status(200).json('File uploded successfully');
+    return res.status(200).json("File uploded successfully");
   } catch (error) {
     console.error(error);
   }
 });
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
-  console.log('Backend server is running!');
+  console.log("Backend server is running!");
 });
